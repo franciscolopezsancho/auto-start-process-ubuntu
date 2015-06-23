e9394d1fb21110c84d2a6a2018dc4ac5c6bad35c
franciscolopezsancho <franciscolopezsancho@gmail.com>
2015-06-23 12:30:24 +0100

    making call to sh a deamon process

--- is-amid-up.sh
+++ is-amid-up.sh
@@ -11,6 +11,6 @@ if [ $isUp = false ];then
 echo "amid-rest API has found down at $(date)" >> amid-rest-cheker.log
 myIp=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
 echo "amid rest API has been found down and has been restarted. If everything went ok this should work: http://$myIp:3000/test/fingerprinting?limit=1" | mail  -s "amid-rest API up?" francisco.lopezsancho@ifpi.org
-nohup /usr/bin/node /node_modules/amid/bin/amid-rest 
+nohup /usr/bin/node /node_modules/amid/bin/amid-rest &
 fi
 
